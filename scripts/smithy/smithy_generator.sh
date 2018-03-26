6cf4b31f22873755eb722b6d225488e7e78ba120
Brian Shannan <brian.shannan@workiva.com>
2018-03-26 18:59:11 -0500

    add glide install to smithy tests

--- scripts/smithy/smithy_generator.sh
+++ scripts/smithy/smithy_generator.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 set -e
 
+which glide > /dev/null || {
+    curl https://glide.sh/get | sh
+}
+
 # Run the generator tests
 cd $FRUGAL_HOME
+glide install
 go build -o frugal
 go test -race ./test
 mv frugal $SMITHY_ROOT
